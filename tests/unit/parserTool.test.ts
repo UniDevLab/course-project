19dc9ef27db3b960e254a146dcf976acbbc07ff2
Dzakhangir Kalantarli <dzakhanhir.k@atlasapps.eu>
2023-12-25 23:18:38 +0100

    feat: Added mockable data of parser

---
+++ tests/unit/parserTool.test.ts
@@ -0,0 +1,22 @@
+// parserTool.test.js
+import { ParserTool } from "../../src/tools/parser.tool";
+import { describe, expect, test, beforeEach } from "@jest/globals";
+import { readFile } from "../tools/reader.tool";
+import { Asset } from "../../src/types/external/assets/assets.types";
+
+const pathToFile = "tests/data/parserTool.json";
+const { assets, results } = readFile(pathToFile);
+
+describe("ParserTool", () => {
+  let parserTool: ParserTool;
+
+  beforeEach(() => {
+    parserTool = new ParserTool();
+  });
+
+  test("parse method should return translated schema", () => {
+    const parsed = assets.map((asset: Asset) => parserTool.parse(asset));
+
+    expect(parsed).toEqual(results);
+  });
+});
